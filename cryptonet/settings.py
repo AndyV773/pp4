a795e7f82154336a387094f8ffc45ae526ee726b
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2025-01-06 14:04:57 +0000

    setup for deployment

--- cryptonet/settings.py
+++ cryptonet/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nqcp%kfybmhtn62e0by#u(_@(1s58tmb^((m0fzv#@t#=3o1ro'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-andyv773-pp4-fxtw79b6ioc.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-andyv773-pp4-fxtw79b6ioc.ws.codeinstitute-ide.net',
+                 '.herokuapp.com']
 
 
 # Application definition
