5fd0f391b1654b44cc2ebd36d9983cdff12c86ae
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2025-01-07 10:15:45 +0000

    Add base.html template and app level templates

--- crypto/models.py
+++ crypto/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Channel(models.Model):
+    """
+    Stores a single channel
+    """
     name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     created_on = models.DateTimeField(auto_now_add=True)
--- crypto/views.py
+++ crypto/views.py
@@ -4,6 +4,15 @@ from django.http import HttpResponse
 from .models import Channel
 
 # Create your views here.
+class ChannelList(generic.ListView):
+    """
+    View to display a list of channels
+    """
+    # all().order_by("rank")
+    queryset = Channel.objects.all()
+    template_name = "crypto/index.html"
+
+
 def my_channel(request):
     return HttpResponse("Hello World!")
 
