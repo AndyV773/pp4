e1168be28323a6afd46f2615d6bb27505a74d502
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2025-01-08 13:48:46 +0000

    Add styles for login and logout templates

--- cryptonet/settings.py
+++ cryptonet/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-andyv773-pp4-fxtw79b6ioc.ws.codeinstitute-ide.net',
                  '.herokuapp.com']
