2cfe3e25b5bf9d90a0becb05ffd3b78e813aa53b
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2025-01-08 13:12:45 +0000

    Add sidenav functionality for diffrent screen sizes, and styles for both menu and channel buttons

--- static/js/sidenav.js
+++ static/js/sidenav.js
@@ -1,7 +1,30 @@
+// code from w3s https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav_full
 function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
+    let open = document.getElementById("mySidenav");
+
+    if (open.style.width === "70%") {
+        document.getElementById("mySidenav").style.width = "0";
+    } else if (window.innerWidth >= 576) {
+        document.getElementById("mySidenav").style.width = "70%";
+    } else {
+        document.getElementById("mySidenav").style.width = "100%";
+    }
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+function menuButtonDisplay() {
+
+    if (window.innerWidth >= 576) {
+        document.getElementById("menu-button").innerHTML = "Menu";
+    } else {
+        document.getElementById("menu-button").innerHTML = "&#9776;";
+    }
+}
+
+menuButtonDisplay()
+
+// resize event https://www.w3schools.com/jsref/event_onresize.asp
+window.addEventListener("resize", menuButtonDisplay);
