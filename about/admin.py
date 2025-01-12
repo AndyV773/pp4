e7aa081462d616c1b8098cf7116de960618be60e
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2025-01-12 13:42:49 +0000

    Add about contact request model and admin panel views

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import About, ContactRequest
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+    """
+    Admin configuration for the About model, with Summernote integration
+    for rich text.
+    """
+    list_display = ('title', 'updated_on')
+    summernote_fields = ('content',)
+
+@admin.register(ContactRequest)
+class ContactRequestAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the ContactRequest model.
+    """
+    list_display = ('message', 'read', 'created_on')
+    list_filter = ('read',)
--- about/models.py
+++ about/models.py
@@ -11,3 +11,16 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
+
+class ContactRequest(models.Model):
+    """
+    Stores a single collaboration request message.
+    """
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField(max_length=280)
+    read = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Contact request from {self.name}"
