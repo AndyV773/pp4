9306cdb00a897aaa8aacc4b78fc7849a9cba2872
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2025-01-14 12:20:58 +0000

    collectstatic

--- staticfiles/js/posts.js
+++ staticfiles/js/posts.js
@@ -1,7 +1,9 @@
+const editModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const editButtons = document.getElementsByClassName("btn-edit");
-const postText = document.getElementById("id_content");
-const postForm = document.getElementById("postForm");
-const submitButton = document.getElementById("submitButton");
+
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
 
 
 /**
@@ -18,8 +20,32 @@ for (let button of editButtons) {
     button.addEventListener("click", (e) => {
         let postId = e.target.getAttribute("data-post_id");
         let postContent = document.getElementById(`post${postId}`).innerText;
+
+        let postText = document.getElementById(`id_content${postId}`);
         postText.value = postContent;
+
+        const submitButton = document.getElementById(`submitButton${postId}`);
         submitButton.innerText = "Update";
+
+        let postForm = document.getElementById(`postForm${postId}`);
         postForm.setAttribute("action", `edit_post/${postId}`);
+    });
+}
+
+/**
+* Initializes deletion functionality for the provided delete buttons.
+* 
+* For each button in the `deleteButtons` collection:
+* - Retrieves the associated comment's ID upon click.
+* - Updates the `deleteConfirm` link's href to point to the 
+* deletion endpoint for the specific comment.
+* - Displays a confirmation modal (`deleteModal`) to prompt 
+* the user for confirmation before deletion.
+*/
+for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+        let postId = e.target.getAttribute("data-post_id");
+        deleteConfirm.href = `delete_post/${postId}`;
+        deleteModal.show();
     });
 }
